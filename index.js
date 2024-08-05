4cfbd4eb09262142eee66707fa490ea10e321c64
Luis Machado <luismachadopnf@gmail.com>
2024-08-05 11:17:45 +0200

    Add HTML index and JS file for loading the DSA modules

---
+++ index.js
@@ -0,0 +1,18 @@
+import { LinkedList } from "/data-structures/linked-lists/singly-linked-list.js";
+
+const list = new LinkedList();
+list.push(10);
+list.push(20);
+list.push(30);
+list.push(40);
+
+// list.pop();
+// list.unshift(5);
+// list.shift();
+
+console.log(list);
+
+console.log("findAt", list.findAt(3));
+console.log("find", list.find(10));
+
+console.log("private property size", list.tail);
